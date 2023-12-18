19a30638f1009bd5c3a7bbe119361ffcf4d0d9ab
Yunjidev <florian.vancamp@gmail.com>
2023-12-18 17:33:54 +0100

    remove mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   attr_accessor :token
-  after_create :welcome_send
   after_create :create_jwt_and_cart
   has_one_attached :avatar
   
@@ -27,16 +26,8 @@ class User < ApplicationRecord
     Cart.create(user: self) #Permt de créer un panier aprés au même moment que l'inscription
   end
   
-  # def welcome_send
-  #   UserMailer.welcome_email(self).deliver_now
-  # end
-  
   has_many :orders, dependent: :destroy
   has_one :cart, dependent: :destroy
-
-  def welcome_send
-    UserMailer.welcome_email(self).deliver_now
-  end
 
 end
 
